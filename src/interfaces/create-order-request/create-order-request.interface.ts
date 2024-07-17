d1a6e75d5caeebabf432a7fb48cde4a201095b1d
Guilherme <guisou1902@gmail.com>
2024-07-17 20:41:24 -0300

    fix: changed requester phone field number to string

--- src/interfaces/create-order-request/create-order-request.interface.ts
+++ src/interfaces/create-order-request/create-order-request.interface.ts
@@ -1,11 +1,11 @@
-export interface IRequest{
+export interface IRequest {
 	subject: string;
 	requesterName: string;
-	requesterPhone: number;
-	requesterStreet: string
-	requesterHouseNumber: number
-	requesterComplement: string
-	requesterZipcode: string
-	expirationDate: string
-	notes: string
+	requesterPhone: string;
+	requesterStreet: string;
+	requesterHouseNumber: number;
+	requesterComplement: string;
+	requesterZipcode: string;
+	expirationDate: string;
+	notes: string;
 }

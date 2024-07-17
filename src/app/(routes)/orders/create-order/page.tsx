a33a2cf4cc012f0b76e602f0743778ffc90ad4e6
Rafael da Conceição <mnraffaell@gmail.com>
2024-07-17 01:50:25 -0300

    refact(page): refact form create-order

--- src/app/(routes)/orders/create-order/page.tsx
+++ src/app/(routes)/orders/create-order/page.tsx
@@ -1,42 +1,32 @@
 "use client";
-import { Input } from "@/components/input";
 import Link from "next/link";
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent } from "react";
+import { IRequest } from "@/interfaces/create-order-request/create-order-request.interface";
+
 
 export default function Page() {
-	const subjectRef = useRef<HTMLInputElement>(null);
-	const requesterNameRef = useRef<HTMLInputElement>(null);
-	const requesterPhoneRef = useRef<HTMLInputElement>(null);
-	const requesterStreetRef = useRef<HTMLInputElement>(null);
-	const requesterHouseNumberRef = useRef<HTMLInputElement>(null);
-	const requesterComplementRef = useRef<HTMLInputElement>(null);
-	const requesterZipcodeRef = useRef<HTMLInputElement>(null);
-	const expirationDateRef = useRef<HTMLInputElement>(null);
-	const notesRef = useRef<HTMLTextAreaElement>(null);
+
+	const request: IRequest = {} as IRequest
 
 	const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log(subjectRef.current?.value);
-		const formattedDate = new Date(
-			expirationDateRef.current?.value
-		).toISOString();
-		const houseNumber = Number(requesterHouseNumberRef.current?.value);
+		const values = e.currentTarget.children
+		const numberValues = e.currentTarget.childElementCount
+		
+		for(let i= 0; i < numberValues; i++){
+			const input = values[i] as HTMLInputElement;
+			if(input.name){
+				//@ts-expect-error 
+				request[input.name] = input.value
+			}
+		}
+
 		await fetch("http://localhost:8080/api/order/create-order", {
 			method: "POST",
 			headers: {
 				"Content-type": "application/json",
 			},
-			body: JSON.stringify({
-				subject: subjectRef.current?.value,
-				requesterName: requesterNameRef.current?.value,
-				requesterPhone: requesterPhoneRef.current?.value,
-				requesterStreet: requesterStreetRef.current?.value,
-				requesterHouseNumber: houseNumber,
-				requesterComplement: requesterComplementRef.current?.value,
-				requesterZipcode: requesterZipcodeRef.current?.value,
-				expirationDate: formattedDate,
-				notes: notesRef.current?.value,
-			}),
+			body: JSON.stringify(request),
 		})
 			.then((res) => {
 				if (res.ok) {
@@ -63,58 +53,49 @@ export default function Page() {
 				>
 					Voltar
 				</Link>
-				<form onSubmit={onSubmit} className="mt-4 flex flex-wrap">
-					<input
-						type="text"
-						name="subject"
-						placeholder="Assunto"
-						ref={subjectRef}
-					/>
+				<form action='#' onSubmit={(e)=>onSubmit(e)} className="mt-4 flex flex-wrap">
+						<input
+							type="text"
+							name="subject"
+							placeholder="Assunto"
+						/>
 					<input
 						type="text"
 						name="requesterName"
 						placeholder="Nome do solicitante"
-						ref={requesterNameRef}
 					/>
 					<input
 						type="tel"
 						name="requesterPhone"
 						placeholder="Telefone do solicitante"
-						ref={requesterPhoneRef}
 					/>
 					<input
 						type="text"
-						name="requesterStreet"
+						name="requesterPhone"
 						placeholder="Endereço do solicitante"
-						ref={requesterStreetRef}
 					/>
 					<input
 						type="number"
 						name="requesterHouseNumber"
 						placeholder="N° da casa do solicitante"
-						ref={requesterHouseNumberRef}
 					/>
 					<input
 						type="text"
 						name="requesterComplement"
 						placeholder="Complemento do solicitante"
-						ref={requesterComplementRef}
 					/>
 					<input
 						type="text"
 						name="requesterZipcode"
 						placeholder="CEP do solicitante"
-						ref={requesterZipcodeRef}
 					/>
 					<input
 						type="date"
 						name="expirationDate"
-						ref={expirationDateRef}
 					/>
 					<textarea
 						name="notes"
 						placeholder="Observações"
-						ref={notesRef}
 					/>
 					<button
 						className="bg-[#7F56D8] text-white font-medium rounded px-4 py-2 hover:-translate-y-1 transition-all w-full"

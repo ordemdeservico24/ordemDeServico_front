871a1e59d239b73da7c573b61801aa15ae455955
Guilherme <guisou1902@gmail.com>
2024-07-18 17:05:39 -0300

    feat: added the container component, improove the table to turn responsible

--- src/app/(routes)/teams/page.tsx
+++ src/app/(routes)/teams/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Container } from "@/components/container";
 import { ITeam } from "@/interfaces/team.interfaces";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -26,60 +27,74 @@ export default function Page() {
 	return (
 		<div>
 			<h1 className="font-semibold text-xl">Equipes</h1>
-			<div className="mt-4 bg-white min-w-full p-4 rounded-lg flex flex-col gap-4">
-				<Link
-					href="/teams/create"
-					className="bg-[#7F56D8] text-white w-fit font-medium rounded px-4 py-2 hover:-translate-y-1 transition-all"
-				>
-					Criar equipe
-				</Link>
-				<table className="px-8 py-4 min-w-full divide-y divide-gray-200 rounded-xl bg-white">
-					<thead>
-						<tr className="text-left">
-							<th>Nome da equipe</th>
-							<th>Líder da equipe</th>
-							<th>Ordens atribuídas</th>
-							<th>Membros na equipe</th>
-						</tr>
-					</thead>
-					<tbody>
-						{teams.map((team, index) => (
-							<tr
-								key={index}
-								className="border-b cursor-pointer hover:bg-gray-100"
-							>
-								<td className="py-4">
-									<Link href={`/teams/${team.id}`} passHref>
-										<span className="block w-full h-full">
-											{team.teamName}
-										</span>
-									</Link>
-								</td>
-								<td className="py-4">
-									<Link href={`/teams/${team.id}`} passHref>
-										<span className="block w-full h-full">
-											{team.leader.name}
-										</span>
-									</Link>
-								</td>
-								<td className="py-4">
-									<Link href={`/teams/${team.id}`} passHref>
-										<span className="block w-full h-full">
-											{team.orders.length}
-										</span>
-									</Link>
-								</td>
-								<td className="py-4">
-									<Link href={`/teams/${team.id}`} passHref>
-										<span className="block w-full h-full">
-											{team.members.length}
-										</span>
-									</Link>
-								</td>
+			<div className="max-w-full overflow-x-auto">
+				<Container className="overflow-x-auto">
+					<Link
+						href="/teams/create"
+						className="bg-[#7F56D8] text-white w-fit font-medium rounded px-4 py-2 hover:-translate-y-1 transition-all"
+					>
+						Criar equipe
+					</Link>
+					<table className="px-8 py-4 min-w-full divide-y divide-gray-200 rounded-xl bg-white">
+						<thead>
+							<tr className="text-left whitespace-nowrap">
+								<th className="pr-6">Nome da equipe</th>
+								<th className="pr-6">Líder da equipe</th>
+								<th className="pr-6">Ordens atribuídas</th>
+								<th className="pr-6">Membros na equipe</th>
 							</tr>
-						))}
-					</tbody>
-				</table>
+						</thead>
+						<tbody>
+							{teams.map((team, index) => (
+								<tr
+									key={index}
+									className="border-b cursor-pointer hover:bg-gray-100 whitespace-nowrap"
+								>
+									<td className="py-4 pr-6">
+										<Link
+											href={`/teams/${team.id}`}
+											passHref
+										>
+											<span className="block w-full h-full">
+												{team.teamName}
+											</span>
+										</Link>
+									</td>
+									<td className="py-4 pr-6">
+										<Link
+											href={`/teams/${team.id}`}
+											passHref
+										>
+											<span className="block w-full h-full">
+												{team.leader.name}
+											</span>
+										</Link>
+									</td>
+									<td className="py-4 pr-6">
+										<Link
+											href={`/teams/${team.id}`}
+											passHref
+										>
+											<span className="block w-full h-full">
+												{team.orders.length}
+											</span>
+										</Link>
+									</td>
+									<td className="py-4 pr-6">
+										<Link
+											href={`/teams/${team.id}`}
+											passHref
+										>
+											<span className="block w-full h-full">
+												{team.members.length}
+											</span>
+										</Link>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</Container>
 			</div>
 		</div>
 	);

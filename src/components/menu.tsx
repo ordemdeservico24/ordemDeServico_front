3431327593b45b6c046742df83d3b7cbe4cafb4f
Guilherme <guisou1902@gmail.com>
2024-07-18 11:48:36 -0300

    feat: added a dropdown menu, on 'equipes' item

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
 import React, { ReactNode } from "react";
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function Menu() {
 	return (
@@ -15,8 +17,31 @@ export default function Menu() {
 						Ordens de serviço
 					</li>
 				</Link>
-				<li className="hover:bg-[rgba(127,86,216,0.1)] px-8 py-4 hover:text-[#7F56D8] cursor-pointer font-medium">
-					Equipes
+				<li className="hover:bg-[rgba(127,86,216,0.1)] px-8 py-4 hover:text-[#7F56D8] cursor-pointer font-medium group">
+					<p className="flex items-center justify-between">
+						Equipes
+						<IoIosArrowDown className="transition-all duration-200 group-hover:rotate-180" />
+					</p>
+					<div className="flex-col pl-4 hidden group-hover:flex gap-4 w-full">
+						<Link
+							href="/teams"
+							className="mt-4 hover:text-[#5100ff]"
+						>
+							Ver equipes
+						</Link>
+						<Link
+							href="/teams/leaders"
+							className="hover:text-[#5100ff]"
+						>
+							Líderes
+						</Link>
+						<Link
+							href="/teams/members"
+							className="hover:text-[#5100ff]"
+						>
+							Membros
+						</Link>
+					</div>
 				</li>
 				<li className="hover:bg-[rgba(127,86,216,0.1)] px-8 py-4 hover:text-[#7F56D8] cursor-pointer font-medium">
 					Usuários

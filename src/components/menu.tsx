3223e77fe6e521ac310d4fb8d250ac94bb8cb8be
Guilherme <guisou1902@gmail.com>
2024-07-19 20:29:05 -0300

    fix: fixed bug menu on desktop, it was with display hidden on desktop

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -33,7 +33,7 @@ export default function Menu() {
 			)}
 			<div
 				className={`border-x border-[#d7d7d7] shadow-inner absolute transition-all duration-200 ${
-					mobileMenu ? "bg-[#ffffff]" : "-left-52"
+					mobileMenu ? "bg-[#ffffff]" : "-left-52 lg:left-0"
 				} h-full lg:relative lg:inline`}
 			>
 				<ul className="flex flex-col mt-8 gap-4">

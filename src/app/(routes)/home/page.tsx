5d5695051bac99b4bc878ab29ae343c0c6d60a18
Guilherme <guisou1902@gmail.com>
2024-08-26 23:54:27 -0300

    feat: added data.orders on Array.isArray validation

--- src/app/(routes)/home/page.tsx
+++ src/app/(routes)/home/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { Container } from "@/components/container";
 import { useEffect, useState } from "react";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Tabs, TabsContent } from "@/components/ui/tabs"
-import { Search } from "lucide-react"
+import {
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { useStore } from "../../../zustandStore";
-import { getCookie } from 'cookies-next';
+import { getCookie } from "cookies-next";
 interface Order {
 	id: string;
 	orderId: number;
@@ -36,7 +41,7 @@ export default function Home() {
 	const [orders, setOrders] = useState<Order[]>([]);
 
 	const { name } = useStore();
-	const token = getCookie('access_token');
+	const token = getCookie("access_token");
 
 	useEffect(() => {
 		fetch(
@@ -51,8 +56,8 @@ export default function Home() {
 		)
 			.then((res) => res.json())
 			.then((data) => {
-				if (Array.isArray(data)) {
-					setOrders(data);
+				if (Array.isArray(data.orders)) {
+					setOrders(data.orders);
 				} else {
 					console.error("Resposta da API não é um array:", data);
 				}
@@ -94,53 +99,74 @@ export default function Home() {
 					<TabsContent value="all">
 						<Card x-chunk="dashboard-06-chunk-0">
 							<CardHeader>
-							<CardTitle className="text-[#3b82f6] text-2xl font-bold">
-                                    Seja bem-vindo, {name || 'Usuário'}.
-                                </CardTitle>
-								<CardDescription>Cheque todas as informações, navegue por todas as abas e acostume-se com esse gerenciamento de tarefas. Tudo didático e aprimorado. Abaixo, cheque algumas informações sobre o que você terá acesso aqui na plataforma:</CardDescription>
-							  <div className="flex items-center justify-between">
-								
-							  <div className="relative flex-1 md:grow-0">
-									<Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-									<Input
-										type="search"
-										placeholder="Pesquisar..."
-										className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-									/>
-								</div>
+								<CardTitle className="text-[#3b82f6] text-2xl font-bold">
+									Seja bem-vindo, {name || "Usuário"}.
+								</CardTitle>
+								<CardDescription>
+									Cheque todas as informações, navegue por
+									todas as abas e acostume-se com esse
+									gerenciamento de tarefas. Tudo didático e
+									aprimorado. Abaixo, cheque algumas
+									informações sobre o que você terá acesso
+									aqui na plataforma:
+								</CardDescription>
+								<div className="flex items-center justify-between">
+									<div className="relative flex-1 md:grow-0">
+										<Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+										<Input
+											type="search"
+											placeholder="Pesquisar..."
+											className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+										/>
+									</div>
 								</div>
 							</CardHeader>
 							<div className="p-3">
-							<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-							<div className="p-4 bg-blue-500 text-white rounded shadow">
-								<h2 className="text-lg font-bold">Total de Ordens</h2>
-								<p className="text-2xl">{totalOrders}</p>
+								<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+									<div className="p-4 bg-blue-500 text-white rounded shadow">
+										<h2 className="text-lg font-bold">
+											Total de Ordens
+										</h2>
+										<p className="text-2xl">
+											{totalOrders}
+										</p>
+									</div>
+									<div className="p-4 bg-green-500 text-white rounded shadow">
+										<h2 className="text-lg font-bold">
+											Ordens em Aberto
+										</h2>
+										<p className="text-2xl">{openOrders}</p>
+									</div>
+									<div className="p-4 bg-yellow-500 text-white rounded shadow">
+										<h2 className="text-lg font-bold">
+											Ordens em Andamento
+										</h2>
+										<p className="text-2xl">
+											{inProgressOrders}
+										</p>
+									</div>
+									<div className="p-4 bg-gray-500 text-white rounded shadow">
+										<h2 className="text-lg font-bold">
+											Ordens Abertas Hoje
+										</h2>
+										<p className="text-2xl">
+											{ordersOpenedToday}
+										</p>
+									</div>
+									<div className="p-4 bg-red-500 text-white rounded shadow">
+										<h2 className="text-lg font-bold">
+											Ordens em Atraso
+										</h2>
+										<p className="text-2xl">
+											{ordersDelayed}
+										</p>
+									</div>
+								</div>
 							</div>
-							<div className="p-4 bg-green-500 text-white rounded shadow">
-								<h2 className="text-lg font-bold">Ordens em Aberto</h2>
-								<p className="text-2xl">{openOrders}</p>
-							</div>
-							<div className="p-4 bg-yellow-500 text-white rounded shadow">
-								<h2 className="text-lg font-bold">Ordens em Andamento</h2>
-								<p className="text-2xl">{inProgressOrders}</p>
-							</div>
-							<div className="p-4 bg-gray-500 text-white rounded shadow">
-								<h2 className="text-lg font-bold">Ordens Abertas Hoje</h2>
-								<p className="text-2xl">{ordersOpenedToday}</p>
-							</div>
-							<div className="p-4 bg-red-500 text-white rounded shadow">
-								<h2 className="text-lg font-bold">Ordens em Atraso</h2>
-								<p className="text-2xl">{ordersDelayed}</p>
-							</div>
-						</div>
-							</div>
-							
-						
 						</Card>
 					</TabsContent>
 				</Tabs>
 			</main>
-			
 		</Container>
 	);
 }

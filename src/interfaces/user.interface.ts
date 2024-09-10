5184178fa4ff793b42af2f2088a5c86457c23be6
Guilherme <guisou1902@gmail.com>
2024-09-09 23:25:18 -0300

    feat: added isTeamLeader, isTeamMember, isEmployee, salary fields to user interface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,34 +1,36 @@
 export interface IUser {
-    id: string;
-    name: string;
-    email: string;
-    phone: string;
-    tertiaryGroupId: string | null;
-    typeOfProfileId: string | null; 
-    role: {
-        roleName: string;
-    };
-    createdAt?: string;
-    updatedAt?: string;
+	id: string;
+	name: string;
+	email: string;
+	phone: string;
+	tertiaryGroupId: string | null;
+	typeOfProfileId: string | null;
+	role: {
+		roleName: string;
+	};
+	isTeamLeader: boolean;
+	isTeamMember: boolean;
+	isEmployee: boolean;
+	salary: number | null;
+	createdAt?: string;
+	updatedAt?: string;
 }
 
-  
-  export interface ITypeOfProfile {
-    id: string;
-    typeOfProfile: string;
-    users: IUser[];
-  }
-  
-  export interface IRole {
-    id: string;
-    roleName: string;
-    permissions: IRolePermission[];
-    roleLevel: string;
-    companyId: string | null;
-  }
-  
-  export interface IRolePermission {
-    operations: string[];
-    resource: string;
-  }
-  
+export interface ITypeOfProfile {
+	id: string;
+	typeOfProfile: string;
+	users: IUser[];
+}
+
+export interface IRole {
+	id: string;
+	roleName: string;
+	permissions: IRolePermission[];
+	roleLevel: string;
+	companyId: string | null;
+}
+
+export interface IRolePermission {
+	operations: string[];
+	resource: string;
+}

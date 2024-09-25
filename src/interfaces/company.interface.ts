fd091baa4ff87cb129dfc2d72cbe87317cdb85d4
Guilherme <guisou1902@gmail.com>
2024-09-25 20:50:46 -0300

    feat: added IPrimaryGroup, ISecondaryGroup, ITertiaryGroup interfaces

--- src/interfaces/company.interface.ts
+++ src/interfaces/company.interface.ts
@@ -1,7 +1,30 @@
+import { IUser } from "./user.interface";
+
 export interface ICompany {
 	id: string;
 	cnpj: string;
 	companyName: string;
 	companyPhoto: string | "";
 	planId: string;
+}
+
+export interface IPrimaryGroup {
+	id?: string;
+	stateName: string;
+	companyId?: string;
+	secondaries?: ISecondaryGroup[];
+}
+
+export interface ISecondaryGroup {
+	id?: string;
+	cityName: string;
+	primaryGroupId?: string;
+	tertiaries?: ITertiaryGroup[];
+}
+
+export interface ITertiaryGroup {
+	id?: string;
+	districtName: string;
+	secondaryGroupId?: string;
+	users?: IUser[];
 }

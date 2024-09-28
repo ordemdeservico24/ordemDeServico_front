9e0a523aaf0b0278b187e94c53c38a4522450fe9
Guilherme <guisou1902@gmail.com>
2024-09-28 00:42:28 -0300

    feat: changed name field on ISupplier to supplierName

--- src/interfaces/stock.interface.ts
+++ src/interfaces/stock.interface.ts
@@ -1,19 +1,17 @@
 export interface IStockItem {
-    id: string; 
-    productName: string;
-    quantity: number;
-    unitOfMeasurement: string;
-    productValue: number;
-    supplierId: string;
-    supplier?: ISupplier; 
-    totalMeasurement: number;
-  }
-  
-export interface ISupplier {
-    id: string;
-    name: string;
-    email: string;
-    phone: string;
-  }
+	id: string;
+	productName: string;
+	quantity: number;
+	unitOfMeasurement: string;
+	productValue: number;
+	supplierId: string;
+	supplier?: ISupplier;
+	totalMeasurement: number;
+}
 
-  
+export interface ISupplier {
+	id: string;
+	supplierName: string;
+	email: string;
+	phone: string;
+}

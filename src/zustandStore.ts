289263a62c4fcc3fb2b0df71c843a8934077d058
Guilherme <guisou1902@gmail.com>
2024-09-29 13:03:43 -0300

    feat: added userId field

--- src/zustandStore.ts
+++ src/zustandStore.ts
@@ -1,107 +1,117 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
-import { setCookie, getCookie, deleteCookie } from 'cookies-next';
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { setCookie, getCookie, deleteCookie } from "cookies-next";
 
 export interface Role {
-  id: string;
-  operations: string[];
-  resource: string;
-  roleId: string;
+	id: string;
+	operations: string[];
+	resource: string;
+	roleId: string;
 }
 
 interface User {
-  name: string;
-  profilePicture: string;
-  token: string;
-  role: Role[];
+	name: string;
+	userId: string;
+	profilePicture: string;
+	token: string;
+	role: Role[];
 }
 
 interface StoreState {
-  token: string;
-  name: string;
-  profilePicture: string;
-  role: Role[];
-  setToken: (token: string) => void;
-  setName: (name: string) => void;
-  setProfilePicture: (profilePicture: string) => void;
-  setRole: (role: Role[]) => void;
-  setUser: (user: User) => void;
-  loadUserFromStorage: () => void;
-  logout: () => void;
+	token: string;
+	name: string;
+	userId: string;
+	profilePicture: string;
+	role: Role[];
+	setToken: (token: string) => void;
+	setName: (name: string) => void;
+	setUserId: (userId: string) => void;
+	setProfilePicture: (profilePicture: string) => void;
+	setRole: (role: Role[]) => void;
+	setUser: (user: User) => void;
+	loadUserFromStorage: () => void;
+	logout: () => void;
 }
 
 export const useStore = create<StoreState>()(
-  persist(
-    (set) => ({
-      token: '',
-      name: '',
-      profilePicture: '',
-      role: [],
+	persist(
+		(set) => ({
+			token: "",
+			name: "",
+			userId: "",
+			profilePicture: "",
+			role: [],
 
-      setToken: (token) => {
-        set({ token });
-        setCookie('access_token', token, { maxAge: 7 * 24 * 60 * 60, path: '/' });
-      },
+			setToken: (token) => {
+				set({ token });
+				setCookie("access_token", token, { maxAge: 7 * 24 * 60 * 60, path: "/" });
+			},
 
-      setName: (name) => {
-        set({ name });
-        localStorage.setItem('user_name', name);
-      },
+			setName: (name) => {
+				set({ name });
+				localStorage.setItem("user_name", name);
+			},
 
-      setProfilePicture: (profilePicture) => {
-        set({ profilePicture });
-        localStorage.setItem('profile_picture', profilePicture);
-      },
+			setUserId: (userId) => {
+				set({ userId });
+				localStorage.setItem("userId", userId);
+			},
 
-      setRole: (role: Role[]) => {
-        set({ role });
-        localStorage.setItem('role', JSON.stringify(role));
-      },
+			setProfilePicture: (profilePicture) => {
+				set({ profilePicture });
+				localStorage.setItem("profile_picture", profilePicture);
+			},
 
-      setUser: (user) => {
-        set({
-          token: user.token,
-          name: user.name,
-          profilePicture: user.profilePicture,
-          role: user.role,
-        });
-        setCookie('access_token', user.token, { maxAge: 7 * 24 * 60 * 60, path: '/' });
-        localStorage.setItem('user_name', user.name);
-        localStorage.setItem('profile_picture', user.profilePicture);
-        localStorage.setItem('role', JSON.stringify(user.role));
-      },
+			setRole: (role: Role[]) => {
+				set({ role });
+				localStorage.setItem("role", JSON.stringify(role));
+			},
 
-      loadUserFromStorage: () => {
-        const token = getCookie('access_token')?.toString() || '';
-        const name = localStorage.getItem('user_name') || '';
-        const profilePicture = localStorage.getItem('profile_picture') || '';
-        const role = localStorage.getItem('role');
-        const parsedRole = role ? JSON.parse(role) : [];
+			setUser: (user) => {
+				set({
+					token: user.token,
+					name: user.name,
+					profilePicture: user.profilePicture,
+					role: user.role,
+				});
+				setCookie("access_token", user.token, { maxAge: 7 * 24 * 60 * 60, path: "/" });
+				localStorage.setItem("user_name", user.name);
+				localStorage.setItem("profile_picture", user.profilePicture);
+				localStorage.setItem("role", JSON.stringify(user.role));
+			},
 
-        set({ token, name, profilePicture, role: parsedRole });
-      },
+			loadUserFromStorage: () => {
+				const token = getCookie("access_token")?.toString() || "";
+				const name = localStorage.getItem("user_name") || "";
+				const profilePicture = localStorage.getItem("profile_picture") || "";
+				const role = localStorage.getItem("role");
+				const parsedRole = role ? JSON.parse(role) : [];
 
-      logout: () => {
-        set({
-          token: '',
-          name: '',
-          profilePicture: '',
-          role: [],
-        });
-        deleteCookie('access_token', { path: '/' });
-        localStorage.removeItem('user_name');
-        localStorage.removeItem('profile_picture');
-        localStorage.removeItem('role');
-      },
-    }),
-    {
-      name: 'user-storage',
-      getStorage: () => localStorage,
-      partialize: (state) => ({
-        name: state.name,
-        profilePicture: state.profilePicture,
-        role: state.role,
-      }),
-    }
-  )
+				set({ token, name, profilePicture, role: parsedRole });
+			},
+
+			logout: () => {
+				set({
+					token: "",
+					name: "",
+					profilePicture: "",
+					role: [],
+				});
+				deleteCookie("access_token", { path: "/" });
+				localStorage.removeItem("user_name");
+				localStorage.removeItem("profile_picture");
+				localStorage.removeItem("role");
+			},
+		}),
+		{
+			name: "user-storage",
+			getStorage: () => localStorage,
+			partialize: (state) => ({
+				name: state.name,
+				userId: state.userId,
+				profilePicture: state.profilePicture,
+				role: state.role,
+			}),
+		}
+	)
 );

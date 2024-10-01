d97ab38a0176c09829f4d31e0111c22ab8e70e77
Guilherme <guisou1902@gmail.com>
2024-10-01 01:49:21 -0300

    style: changed padding left and right to padding

--- src/app/(routes)/teams/[id]/page.tsx
+++ src/app/(routes)/teams/[id]/page.tsx
@@ -3,37 +3,22 @@ import { Container } from "@/components/container";
 import { ITeam } from "@/interfaces/team.interfaces";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Tabs, TabsContent } from "@/components/ui/tabs";
-import {
-	Card,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { getCookie } from "cookies-next";
 export default function Page({ params }: { params: { id: string } }) {
 	const [team, setTeam] = useState<ITeam>();
 	const token = getCookie("access_token");
 
 	useEffect(() => {
-		fetch(
-			`https://ordemdeservicosdev.onrender.com/api/team/get-team/${params.id}`,
-			{
-				method: "GET",
-				headers: {
-					"Content-type": "application/json",
-					Authorization: `Bearer ${token}`,
-				},
-			}
-		)
+		fetch(`https://ordemdeservicosdev.onrender.com/api/team/get-team/${params.id}`, {
+			method: "GET",
+			headers: {
+				"Content-type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		})
 			.then((res) => {
 				const status = res.status;
 				return res.json().then((data) => ({ status, data }));
@@ -62,13 +47,8 @@ export default function Page({ params }: { params: { id: string } }) {
 					<TabsContent value="all">
 						<Card x-chunk="dashboard-06-chunk-0">
 							<CardHeader>
-								<CardTitle className="text-[#3b82f6] text-2xl font-bold">
-									Ordens Atribuídas
-								</CardTitle>
-								<CardDescription>
-									Cheque todas as informações relacionado aos
-									líderes apresentados.
-								</CardDescription>
+								<CardTitle className="text-[#3b82f6] text-2xl font-bold">Ordens Atribuídas</CardTitle>
+								<CardDescription>Cheque todas as informações relacionado aos líderes apresentados.</CardDescription>
 							</CardHeader>
 
 							<div className="flex px-4 gap-2 sm:gap-4 flex-wrap">
@@ -79,15 +59,8 @@ export default function Page({ params }: { params: { id: string } }) {
 										href={`/orders/order/${order.id}`}
 									>
 										<div className="flex flex-col gap-2">
-											<p className="font-semibold text-sm sm:text-base">
-												{order.subject.name}
-											</p>
-											<p className="text-xs sm:text-sm">
-												{truncateNotes(
-													order.notes,
-													100
-												)}
-											</p>
+											<p className="font-semibold text-sm sm:text-base">{order.subject.name}</p>
+											<p className="text-xs sm:text-sm">{truncateNotes(order.notes, 100)}</p>
 										</div>
 										<p className="border-t border-white text-xs sm:text-sm pt-2 text-right">
 											{formattedDates(order.openningDate)}
@@ -96,10 +69,8 @@ export default function Page({ params }: { params: { id: string } }) {
 								))}
 							</div>
 
-							<div className="px-4">
-								<p>
-									Membros na equipe: ({team?.members.length})
-								</p>
+							<div className="p-4">
+								<p>Membros na equipe: ({team?.members.length})</p>
 								<Table className="overflow-x-auto min-w-full bg-white rounded-xl divide-y divide-gray-200">
 									<TableHeader>
 										<TableRow>
@@ -110,20 +81,12 @@ export default function Page({ params }: { params: { id: string } }) {
 									</TableHeader>
 									<TableBody>
 										{team?.members.map((member, index) => (
-											<TableRow
-												key={index}
-												className="cursor-pointer hover:bg-gray-100"
-											>
+											<TableRow key={index} className="cursor-pointer hover:bg-gray-100">
 												<TableCell>
 													<p>{member.user.name}</p>
 												</TableCell>
 												<TableCell>
-													<p>
-														{
-															member.user.role
-																.roleName
-														}
-													</p>
+													<p>{member.user.role.roleName}</p>
 												</TableCell>
 												<TableCell>
 													<p>{member.user.phone}</p>

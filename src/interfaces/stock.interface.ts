eb570edccc640d8ff0c3d9093cee383ef9d899bd
Henrique <henriquepinheiroxavier@gmail.com>
2024-10-03 10:54:56 -0300

    feat: order status with review photo and items

--- src/interfaces/stock.interface.ts
+++ src/interfaces/stock.interface.ts
@@ -7,6 +7,8 @@ export interface IStockItem {
 	supplierId: string;
 	supplier?: ISupplier;
 	totalMeasurement: number;
+	usedQuantity: number;
+	usedMeasurement: number;
 }
 
 export interface ISupplier {
